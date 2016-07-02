d78d326d911acf8d0b8d0461b4ec92c51f34f111
Phillip-Juan van der Berg <[flaapjan@zailab.com]>
2016-07-02 13:28:06 +0200

    fix(builder): add basic styling

--- src/builder/builder.js
+++ src/builder/builder.js
@@ -3,17 +3,27 @@
 import {inject, LogManager} from 'aurelia-framework';
 /*
 */
+import {Router} from 'aurelia-router';
+/*
+*/
 const logger = LogManager.getLogger('Builder');
 
+@inject(Router)
 export class Builder {
   
-  constructor() {
+  constructor(router) {
     
+    this.router = router;
     this.init();
   }
   
   init() {
     
     logger.debug(' builder initialised ');
+  }
+  
+  back() {
+    
+    this.router.navigate('menu');
   }
 }
--- src/menu/menu.js
+++ src/menu/menu.js
@@ -1,19 +1,31 @@
 /*
 */
+import {MenuStore} from './menu.store.js';
+/*
+*/
 import {inject, LogManager} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
 /*
 */
 const logger = LogManager.getLogger('Menu');
 
+@inject(MenuStore, Router)
 export class Menu {
   
-  constructor() {
+  constructor(menuStore, router) {
     
+    this.menuStore = menuStore;
+    this.router = router;
     this.init();
   }
   
   init() {
     
     logger.debug(' menu initialised ');
+  }
+  
+  navigate(step) {
+    
+    this.router.navigate(step);
   }
 }
