89bce2059a19b124905ddf3fac6a5f082f9a5092
Phillip-Juan van der Berg <[flaapjan@zailab.com]>
2016-07-15 21:04:40 +0200

    feat(properties): add background color

--- src/builder/prompts/properties/properties.js
+++ src/builder/prompts/properties/properties.js
@@ -16,14 +16,15 @@ export class Properties {
   heightClass = 'pixel';
   tabs = {
     size: false,
-    background: false,
-    border: true
+    background: true,
+    border: false
   };
-  borderStyle = 'solid';
+  borderStyle = 'none';
 //, 'double', 'groove', 'ridge', 'inset', 'outset' =>  does nothing in chrome
   borderStyles = ['none', 'dotted', 'dashed', 'solid', 'hidden'];
   borderWidth = 1;
   borderColor = '0, 0, 0';
+  backgroundColor = '255, 255, 255';
   
   constructor(dialogController, loggerManager, dispatcher, builderStore) {
     
@@ -43,9 +44,14 @@ export class Properties {
     this.widthClass = element.style.width.indexOf('%') !== -1 ? 'percentage' : 'pixel';
     this.heightClass = element.style.height.indexOf('%') !== -1 ? 'percentage' : 'pixel';
     
+    // size tab
     this.width = parseInt(element.style.width.replace('px', ''));
     this.height = parseInt(element.style.height.replace('px', ''));
     
+    // background tab
+    this.backgroundColor = element.style.background ? element.style.background.substring(element.style.background.lastIndexOf('(') + 1, element.style.background.lastIndexOf(')')) : this.backgroundColor;
+    
+    // border tab
     this.borderWidth = element.style.borderWidth ? parseInt(element.style.borderWidth) : this.borderWidth;
     this.borderStyle = element.style.borderStyle ? element.style.borderStyle : this.borderStyle;
     this.borderColor = element.style.borderColor ? element.style.borderColor.substring(element.style.borderColor.lastIndexOf('(') + 1, element.style.borderColor.lastIndexOf(')')) : this.borderColor;
@@ -74,7 +80,8 @@ export class Properties {
       width: this.width + (this.widthClass === 'pixel' ? 'px' : '%'),
       'border-width': this.borderWidth + 'px',
       'border-style': this.borderStyle,
-      'border-color': 'rgb(' + this.borderColor + ')'
+      'border-color': 'rgb(' + this.borderColor + ')',
+      background: 'rgb(' + this.backgroundColor + ')'
     });
     
     this.controller.ok();
