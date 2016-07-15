56414dec949f3d7bc650d2245b29d951bf5083b3
Phillip-Juan van der Berg <[flaapjan@zailab.com]>
2016-07-15 20:53:04 +0200

    feat(properties): add border

--- src/builder/prompts/properties/properties.js
+++ src/builder/prompts/properties/properties.js
@@ -15,10 +15,15 @@ export class Properties {
   widthClass = 'pixel';
   heightClass = 'pixel';
   tabs = {
-    size: true,
+    size: false,
     background: false,
-    border: false
+    border: true
   };
+  borderStyle = 'solid';
+//, 'double', 'groove', 'ridge', 'inset', 'outset' =>  does nothing in chrome
+  borderStyles = ['none', 'dotted', 'dashed', 'solid', 'hidden'];
+  borderWidth = 1;
+  borderColor = '0, 0, 0';
   
   constructor(dialogController, loggerManager, dispatcher, builderStore) {
     
@@ -40,6 +45,10 @@ export class Properties {
     
     this.width = parseInt(element.style.width.replace('px', ''));
     this.height = parseInt(element.style.height.replace('px', ''));
+    
+    this.borderWidth = element.style.borderWidth ? parseInt(element.style.borderWidth) : this.borderWidth;
+    this.borderStyle = element.style.borderStyle ? element.style.borderStyle : this.borderStyle;
+    this.borderColor = element.style.borderColor ? element.style.borderColor.substring(element.style.borderColor.lastIndexOf('(') + 1, element.style.borderColor.lastIndexOf(')')) : this.borderColor;
   }
 
   view(view) {
@@ -51,12 +60,21 @@ export class Properties {
     
     this.tabs[view] = true;
   }
+
+  selectBorderStyle(style) {
+    
+    this.showBorderStyles = false;
+    this.borderStyle = style;
+  }
   
   update() {
     
     this.dispatcher.dispatch('builder.component.style.change', {
       height: this.height + (this.heightClass === 'pixel' ? 'px' : '%'),
-      width: this.width + (this.widthClass === 'pixel' ? 'px' : '%')
+      width: this.width + (this.widthClass === 'pixel' ? 'px' : '%'),
+      'border-width': this.borderWidth + 'px',
+      'border-style': this.borderStyle,
+      'border-color': 'rgb(' + this.borderColor + ')'
     });
     
     this.controller.ok();
